4b370e87ef5bc004f9c26c0bf8e40f5aa03a4f76
shubham kamble <shubhammkamble32@gmail.com>
2026-05-30 22:10:18 +0530

    add space between two tasks

--- script.js
+++ script.js
@@ -1,48 +1,9 @@
 let task = document.querySelector("input");
 let btn = document.querySelector("button");
 let list = document.querySelector("ul");
+let ts = document.querySelector(".tasks")
 
 
-// btn.addEventListener("click",function(){
-//     let new_task = document.createElement("li");
-//     let new_div=document.createElement("div");
-
-//     let de_btn = document.createElement("button");
-//     let item = document.querySelector(".listItem");
-//     de_btn.innerText = "Delete";
-//     // new_task.append(de_btn);
-//     de_btn.classList.add("Delete");
-//     de_btn.style.backgroundColor = "blueviolet"
-//     de_btn.style.color = "white";
-//   de_btn.style.height = "20px";
-//   de_btn.style.width = "85px";
-//    de_btn.style.borderRadius = "7px";
-
-//     new_task.textContent = task.value;
-//     // document.querySelector("ul").append(new_task);
-
-//     item.append(new_task);
-
-//      item.appendChild(de_btn);
-
-//     task.value = "";
-
-
-
-
-
-
-// });
-
-// list.addEventListener("click", function(ev){
-
-
-//     let per = ev.target.parentElement;
-
-
-
-//         per.remove();
-// })
 
 list.addEventListener("click", function (ev) {
 
@@ -60,7 +21,7 @@ btn.addEventListener("click", function () {
         // task.type = "checkbox";
         let check_box = document.createElement("input");
         check_box.type = "checkbox";
-        let new_div = document.createElement("div");
+        let new_div = document.createElement("ul");
         new_div.classList.add("listItem");
 
         let new_task = document.createElement("li");
@@ -69,9 +30,11 @@ btn.addEventListener("click", function () {
         let de_btn = document.createElement("button");
         de_btn.innerText = "Delete";
 
+        let task_div = document.createElement("div");
+
         de_btn.style.backgroundColor = "blueviolet";
         de_btn.style.color = "white";
-        de_btn.style. padding="2px 12px " ;
+        de_btn.style. padding="2px 26.7px " ;
         de_btn.style. cursor=" pointer ";
         de_btn.style.borderRadius = "7px";
         de_btn.style.display=" inline-flex" ;
@@ -81,15 +44,19 @@ btn.addEventListener("click", function () {
 de_btn.style.alignItems = "center";
 de_btn.style.justifyContent = "center";
         //   de_btn.style.
+        // new_div.style.paddingBlock="8px";
 
 
-
-       
-        new_div.appendChild(new_task);
+// /        task_div.style.backgroundColor="white";
+       task_div.appendChild(new_task)
+        new_div.appendChild(task_div);
         
         new_div.appendChild(de_btn); 
 
-        list.appendChild(new_div);
+        // list.appendChild(new_div);
+
+        ts.appendChild(new_div);
+        
 
         task.value = "";
     }
